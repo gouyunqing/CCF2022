90eee57ba431cfc60050c0f68d2a76761dbfed21
gouyunqing <gouyunqing@outlook.com>
2022-09-12 16:00:01 +0100

    finished process_data.py

--- dataloader.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
-import numpy as np
-from process_data import read_data
-import random
-
-class Dataset(Dataset):
-    def __init__(self):
-
-
--- process_data.py
+++ process_data.py
@@ -1,4 +1,10 @@
 import json
+from keras_preprocessing.sequence import pad_sequences
+from torch.utils.data import DataLoader, TensorDataset, RandomSampler, SequentialSampler
+import numpy as np
+from process_data import read_data
+from transformers import BertTokenizer
+import random
 
 
 def read_data(data_path):
@@ -16,13 +22,14 @@ def read_data(data_path):
     return data_list
 
 
-def load_data(data_list, tokenizer, is_train=True):
+def load_data(data_list, tokenizer, MAX_LEN, is_train=True):
     '''
     讲json的list处理成Bert中的token_idx
     :param data_list: list
     :param is_train: Boolean
-    :return: 一个list，每个元素都是一个长度为1（test）或长度为2（train）的list， [0]位置是token_idx的序列， [1]位置是label
+    :return:
     '''
+    input_pairs = []
     for data in data_list:
         data_id = data['id']
         title = data['title']
@@ -30,5 +37,69 @@ def load_data(data_list, tokenizer, is_train=True):
         abstract = data['abstract']
         if is_train is True:
             label = data['label_id']
+        else:
+            label = '[UNK]'
 
+        text = title + abstract
+        encoded_text = tokenizer.encode(
+            text,
+            add_special_tokens=True
+        )
+        input_pairs.append(encoded_text)
+    input_pairs = pad_sequences(input_pairs, maxlen=MAX_LEN, dtype='long', value=0, truncating='post', padding='post')
+    for i in range(len(input_pairs)):
+        input_pairs[i] = [input_pairs[i], label]
+
+    return input_pairs
+
+
+def generate_mask(input_pairs):
+    '''
+    为每个输入向量生成一个mask向量。所有是padding的位置为False, 所有text位置为True
+    :param input_pairs:
+    :return:mask向量的列表
+    '''
+    attention_masks = []
+    for pair in input_pairs:
+        input_id = pair[0]
+        mask = [int(_) > 0 for _ in input_id]
+        attention_masks.append(mask)
+
+    return attention_masks
+
+
+def build_dataloader(input_pairs, attention_masks, batch_size, is_train=True):
+    '''
+    生成dataloader
+    :param input_pairs:
+    :param attention_masks:
+    :param batch_size:
+    :param is_train:
+    :return:
+    '''
+    input_ids = []
+    labels = []
+    for pair in input_pairs:
+        input_ids.append(pair[0])
+        labels.append(pair[1])
+
+    data = TensorDataset(input_ids, attention_masks, labels)
+    if is_train:
+        sampler = RandomSampler(data)
+    else:
+        sampler = SequentialSampler(data)
+
+    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    return dataloader
+
+
+def pipline(data_path, MAX_LEN, batch_size, is_train=True):
+    data_list = read_data(data_path)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+    input_pairs = load_data(data_list=data_list, tokenizer=tokenizer, is_train=is_train, MAX_LEN=MAX_LEN)
+    attention_masks = generate_mask(input_pairs)
+    dataloader = build_dataloader(input_pairs=input_pairs, attention_masks=attention_masks, batch_size=batch_size,
+                                  is_train=is_train)
+
+    return dataloader
 
